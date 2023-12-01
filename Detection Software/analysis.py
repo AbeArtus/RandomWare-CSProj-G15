dc8315c0a34d11c96e7dc32484b5b12e9828a444
Eli DeBruyne <eli2.debruyne@gmail.com>
2023-12-01 11:04:03 -0600

    cahnged name and output
    output to csv (not fully done yet)
    changed name from keyboardlogger_detection.py to analysis.py

--- Detection Software/analysis.py
+++ Detection Software/analysis.py
@@ -2,20 +2,21 @@ import ast
 import os
 import json
 import glob
+import csv
 
 config_items = {}
 
 class FileOpenVisitor(ast.NodeVisitor):
-    def __init__(self):
-        self.variables = {}
+    def __init__(self, csv_writer):
+        self.csv_writer = csv_writer
 
-    def visit_For(self, node):
-        print(f"For loop detected at line {node.lineno}")
-        self.generic_visit(node)
+    # def visit_For(self, node):
+    #     print(f"For loop detected at line {node.lineno}")
+    #     self.generic_visit(node)
 
-    def visit_While(self, node):
-        print(f"While loop detected at line {node.lineno}")
-        self.generic_visit(node)
+    # def visit_While(self, node):
+    #     print(f"While loop detected at line {node.lineno}")
+    #     self.generic_visit(node)
 
     # Covers base imports
     def visit_Import(self, node):
@@ -25,7 +26,10 @@ class FileOpenVisitor(ast.NodeVisitor):
         self.generic_visit(node)
 
     # Covers 'Import ... from ...'
-    def visit_ImportFrom(self, node):
+    def visit_Import(self, node):
+        for alias in node.names:
+            if alias.name in config_items["Libraries"]:
+                self.csv_writer.writerow(['Import', alias.name, node.lineno])
         module = node.module
         if module in config_items["Libraries"]:
             for name in node.names:
@@ -66,14 +70,16 @@ class FileOpenVisitor(ast.NodeVisitor):
 
 #### difference is this one can detect all functions from any of the imported libraries
     def visit_Call(self, node):
-        if isinstance(node.func, ast.Attribute) and isinstance(node.func.value, ast.Name):
-            # Check if the call is a method
-            if node.func.value.id in config_items["Libraries"]:
-                print(f"Method '{node.func.attr}' from the library '{node.func.value.id}' called at line {node.lineno}")
-            method_call = f"{node.func.value.id}.{node.func.attr}"
-            if method_call in config_items["Functions"]:
-                print(f"Method call '{method_call}' at line {node.lineno}")
+        if isinstance(node.func, ast.Attribute):
+            library_name = None
+            if isinstance(node.func.value, ast.Name):
+                library_name = node.func.value.id  # This is the library or sublibrary name
 
+            full_function_name = f"{library_name}.{node.func.attr}" if library_name else node.func.attr
+            print(f"Function call '{full_function_name}' at line {node.lineno}")
+
+            if full_function_name in config_items["Functions"]:
+                print(f"Tracked function call '{full_function_name}' at line {node.lineno}")
         elif isinstance(node.func, ast.Name):
             # Check for direct function call
             if node.func.id in config_items["Functions"]:
@@ -81,11 +87,25 @@ class FileOpenVisitor(ast.NodeVisitor):
 
         # Check for functions passed as arguments
         for arg in node.args:
-            if isinstance(arg, ast.Name) and arg.id in config_items["Functions"]:
+            if isinstance(arg, ast.Attribute) and isinstance(arg.value, ast.Name):
+                library_or_sublibrary_name = arg.value.id
+                method_name = arg.attr
+                full_function_name = f"{library_or_sublibrary_name}.{method_name}"
+                if full_function_name in config_items["Functions"]:
+                    print(f"Method '{full_function_name}' used as argument at line {node.lineno}")
+
+            elif isinstance(arg, ast.Name) and arg.id in config_items["Functions"]:
                 print(f"Function '{arg.id}' used as argument at line {node.lineno}")
 
         for keyword in node.keywords:
-            if isinstance(keyword.value, ast.Name) and keyword.value.id in config_items["Functions"]:
+            if isinstance(keyword.value, ast.Attribute) and isinstance(keyword.value.value, ast.Name):
+                library_or_sublibrary_name = keyword.value.value.id
+                method_name = keyword.value.attr
+                full_function_name = f"{library_or_sublibrary_name}.{method_name}"
+                if full_function_name in config_items["Functions"]:
+                    print(f"Method '{full_function_name}' used as named argument '{keyword.arg}' at line {node.lineno}")
+
+            elif isinstance(keyword.value, ast.Name) and keyword.value.id in config_items["Functions"]:
                 print(f"Function '{keyword.value.id}' used as named argument '{keyword.arg}' at line {node.lineno}")
 
         self.generic_visit(node)
@@ -107,31 +127,35 @@ def read_config_file(filepath):
         config_items = extract_config_items(data)
 
 def extract_config_items(data):
-    extracted_items = {"Libraries": [], "Functions": []}
+    extracted_items = {"Libraries": {}, "Functions": {}}
     for category in ["Libraries", "Functions"]:
         for subcategory, items in data[category].items():
             for item in items:
-                extracted_items[category].extend(item.keys())
+                for key, value in item.items():
+                    weight = value.get("weight", 50)  # Default weight is 50
+                    extracted_items[category][key] = weight
     return extracted_items
+
 
 def find_weight():
     return
 
-def process_python_file(filename):
+def process_python_file(filename, csv_writer):
     with open(filename, 'r') as file:
         code = file.read()
         tree = ast.parse(code, filename=filename)
-        visitor = FileOpenVisitor()
+        visitor = FileOpenVisitor(csv_writer)
         visitor.visit(tree)
 
 def process_directory(directory_path):
-    # Get a list of all Python files in the directory
     python_files = glob.glob(os.path.join(directory_path, '*.py'))
-    
-    # Process each file
-    for file in python_files:
-        print(f"Processing file: {file}")
-        process_python_file(file)
+
+    with open('output.csv', 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Type', 'Detail', 'Line Number', 'Weight'])
+
+        for file in python_files:
+            process_python_file(file, csv_writer)
 
 
 if __name__ == '__main__':
