232e5b2846e3993590ef94281bef8e0c26357b2e
abeartus <abeartus@ou.edu>
2023-12-02 08:54:49 -0600

    Weights adjucted, graphing added, Merge analysis and weights into one app

--- Detection Software/weight.py
+++ Detection Software/weight.py
@@ -1,14 +1,17 @@
 import csv
 import glob
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 
 def calculate_risk(directory_path):
     # Constants
-    constant_for_saf = 90  # Modify as needed
+    constant_for_saf = 45  # Modify as needed
     threshold = 30
     threshold_forgiveness = 0.5
 
     csv_files = glob.glob(os.path.join(directory_path, '*.csv'))
+    rps = []
     for csv_file in csv_files:
         ml, mf, tl, tf = 0, 0, 0, 0
         threshold_boolean = True
@@ -34,11 +37,12 @@ def calculate_risk(directory_path):
         if (threshold_boolean == True):
             ml = ml * threshold_forgiveness
             mf = mf * threshold_forgiveness
-        saf = 1 + (constant_for_saf / (tl if tl > tf else tf))
+        saf = 1 + (constant_for_saf / (tl if tl > 0 else 1))
 
         # Calculate Risk Percentage (RP)
         rp = ((ml + mf) / (tf if tf > 0 else 1)) * saf
-
+     
+        rps.append(rp)
         print(f"Risk analysis for {os.path.basename(csv_file)}:")
         print(f"  ML (Malicious Libraries): {ml}")
         print(f"  MF (Malicious Functions): {mf}")
@@ -47,9 +51,25 @@ def calculate_risk(directory_path):
         print(f"  SAF (Scale Adjustment Factor): {saf:.2f}")
         print(f"  RP (Risk Percentage): {rp:.2f}%")
         print(f"  Threshold frogiveness starting at weight {threshold} is {threshold_boolean} at {threshold_forgiveness}\n")
+    return rps
 
 # Example usage
 os.chdir(os.path.dirname(__file__))
 os.chdir('../')
-directory_path = os.path.join(os.getcwd(), 'RansomWare/python files')
-calculate_risk(directory_path)
+directory_path = input("Enter the path to the directory containing Python files: ")
+if os.path.isdir(directory_path):
+    rps = calculate_risk(directory_path)
+else:
+    print("Invalid directory path")
+    exit
+
+idx = range(len(rps))
+
+print(f"mean RP: {np.mean(rps)}")
+
+plt.plot(idx, rps, marker = 'o', linestyle = '-')
+plt.title('')
+plt.xlabel('')
+plt.ylabel('')
+plt.show()
+
